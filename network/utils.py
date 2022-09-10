221c9dd77dd622d678464fcf9977cbab19703a56
rohanksaxena <rohansaxena3107@gmail.com>
2022-09-10 14:08:10 +0200

    Add visualization for high and low level features

--- network/utils.py
+++ network/utils.py
@@ -1,15 +1,19 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
 import torch.nn.functional as F
 from collections import OrderedDict
+
 
 class _SimpleSegmentationModel(nn.Module):
     def __init__(self, backbone, classifier):
         super(_SimpleSegmentationModel, self).__init__()
         self.backbone = backbone
         self.classifier = classifier
-        
+
     def forward(self, x):
         input_shape = x.shape[-2:]
         features = self.backbone(x)
@@ -49,16 +53,18 @@ class IntermediateLayerGetter(nn.ModuleDict):
         >>>     [('feat1', torch.Size([1, 64, 56, 56])),
         >>>      ('feat2', torch.Size([1, 256, 14, 14]))]
     """
+
     def __init__(self, model, return_layers, hrnet_flag=False):
         if not set(return_layers).issubset([name for name, _ in model.named_children()]):
             raise ValueError("return_layers are not present in model")
 
         self.hrnet_flag = hrnet_flag
-
+        # print("init backbone: ", model)
         orig_return_layers = return_layers
         return_layers = {k: v for k, v in return_layers.items()}
         layers = OrderedDict()
         for name, module in model.named_children():
+            # print('Init Name: ', name, ' | Module: ', module)
             layers[name] = module
             if name in return_layers:
                 del return_layers[name]
@@ -70,18 +76,51 @@ class IntermediateLayerGetter(nn.ModuleDict):
 
     def forward(self, x):
         out = OrderedDict()
-        for name, module in self.named_children():
-            if self.hrnet_flag and name.startswith('transition'): # if using hrnet, you need to take care of transition
-                if name == 'transition1': # in transition1, you need to split the module to two streams first
+        for name, module in self.named_children():  # 2 modules, ll features and hl features
+            if self.hrnet_flag and name.startswith('transition'):  # if using hrnet, you need to take care of transition
+                if name == 'transition1':  # in transition1, you need to split the module to two streams first
                     x = [trans(x) for trans in module]
-                else: # all other transition is just an extra one stream split
+                else:  # all other transition is just an extra one stream split
                     x.append(module(x[-1]))
-            else: # other models (ex:resnet,mobilenet) are convolutions in series.
+            else:  # other models (ex:resnet,mobilenet) are convolutions in series.
+
                 x = module(x)
+
+                m = nn.ReLU()
+                output = m(x)
+                feature_maps = output.squeeze(0)
+                feature_maps = feature_maps.cpu().numpy()
+                mask = Image.open(
+                    'D:\\Workspaces\\Thesis\\deeplab\\DeepLabV3Plus-Pytorch\\Sooty_Albatross_0031_1066.png')  # Read image mask
+                mask = mask.resize((feature_maps.shape[2],
+                                    feature_maps.shape[1]))  # Reshape mask to spatial resolution of feature maps
+                mask = np.asarray(mask)
+                mask[mask > 0] = 255
+
+                fig = plt.figure(figsize=(feature_maps.shape[1], feature_maps.shape[2]))
+                if name == 'low_level_features':
+                    ncols = 6
+                    nrows = 4
+                elif name == 'high_level_features':
+                    ncols = 20
+                    nrows = 16
+
+                for i, map in enumerate(feature_maps):
+                    map = np.multiply(map, mask)
+                    z = map.reshape(-1)
+                    z = np.float32(z)
+                    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+                    K = 5
+                    ret, label, center = cv2.kmeans(z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+                    label = label.reshape((feature_maps.shape[1], feature_maps.shape[2]))
+                    label = np.multiply(label, mask)
+                    fig.add_subplot(nrows, ncols, i + 1)
+                    plt.imshow(label)
+                plt.show()
 
             if name in self.return_layers:
                 out_name = self.return_layers[name]
-                if name == 'stage4' and self.hrnet_flag: # In HRNetV2, we upsample and concat all outputs streams together
+                if name == 'stage4' and self.hrnet_flag:  # In HRNetV2, we upsample and concat all outputs streams together
                     output_h, output_w = x[0].size(2), x[0].size(3)  # Upsample to size of highest resolution stream
                     x1 = F.interpolate(x[1], size=(output_h, output_w), mode='bilinear', align_corners=False)
                     x2 = F.interpolate(x[2], size=(output_h, output_w), mode='bilinear', align_corners=False)
